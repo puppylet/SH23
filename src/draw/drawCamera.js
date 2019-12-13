8d4e62b4a72f782f301a63af8835ff70419e396e
Nhokchien <chien@mstage.io>
2019-12-13 11:07:20 +0700

    new camera object

--- src/draw/drawCamera.js
+++ src/draw/drawCamera.js
@@ -10,7 +10,7 @@ export default _this => {
   const objLoader = new OBJLoader()
   objLoader.load('camera.obj', object => {
       const cameraGeo = object.children.map(child => child.geometry)
-      const cameraRangeGeometry = new THREE.CylinderGeometry(0, 120, 300, 30, 4)
+      const cameraRangeGeometry = new THREE.BufferGeometry().fromGeometry(new THREE.CylinderGeometry(0, 120, 300, 30, 4))
       // cameraGeo.push(cameraRangeGeometry)
       console.log('camreraGeo', cameraGeo)
       const cameraMergedGeo = BufferGeometryUtils.mergeBufferGeometries(cameraGeo, false)
@@ -32,9 +32,9 @@ export default _this => {
       cameraRange.scale.set(0.7, 0.7, 0.7)
 
       const cameraGroup = new THREE.Group()
-      cameraGroup.add(cameraMesh)
+      // cameraGroup.add(cameraMesh)
       cameraGroup.add(cameraRange)
-      cameraGroup.scale.set(3, 3, 3)
+      // cameraGroup.scale.set(3, 3, 3)
 
 
       const cameraMaterial = new THREE.MeshPhongMaterial({
@@ -48,19 +48,45 @@ export default _this => {
       const cameraInstanceMesh = new THREE.InstancedMesh(cameraMergedGeo, cameraMaterial, cameras.length)
       const cameraTransform = new THREE.Object3D()
       const singleGeo = new THREE.Geometry()
+      const rangeInstance = new THREE.InstancedMesh(cameraRangeGeometry, cameraRangeMaterial, cameras.length)
+      rangeInstance.frustumCulled = false
+      const rangeTransform = new THREE.Object3D()
+
+      rangeTransform.rotateX(-Math.PI / 2)
+      rangeTransform.rotateY(Math.PI / 4)
+      rangeTransform.position.z = 105
+      rangeTransform.scale.set(2, 2, 2)
+
+      rangeTransform.updateMatrix()
+      // rangeTransform.rotateY(0.72)
+      object.scale.set(0.5, 0.5, 0.5)
       cameras.map((camera, index) => {
-        const mesh = cameraGroup.clone()
+        const mesh = object.clone()
         mesh.position.x = camera.x
         mesh.position.z = camera.y
         mesh.position.y = parseFloat(camera.elevation) - 245
 
         mesh.rotateY(-camera.angle)
-        mesh.rotateX(0.72)
-        console.log('mesh camera', mesh)
+        mesh.rotateX(0.18)
+
+
+        rangeTransform.position.x = camera.x
+        rangeTransform.position.z = camera.y
+        rangeTransform.position.y = parseFloat(camera.elevation) - 245
+
+        rangeTransform.rotateZ(-camera.angle || 0)
+
+        rangeTransform.updateMatrix()
+
+        rangeInstance.setMatrixAt(index, rangeTransform.matrix)
+        rangeTransform.rotateZ(camera.angle || 0)
+
+
+        // console.log('mesh camera', mesh)
         // mesh.children[0].updateMatrix()
-        mesh.children[1].updateMatrix()
+        // mesh.children[1].updateMatrix()
         // singleGeo.merge(mesh.children[0].geometry, mesh.children[0].matrix)
-        singleGeo.merge(mesh.children[1].geometry, mesh.children[1].matrix)
+        // singleGeo.merge(mesh.children[1].geometry, mesh.children[1].matrix)
 
         // const rangeGeo = mesh.children[1].geometry
         // const rangeMesh = new THREE.Mesh(rangeGeo)
@@ -68,11 +94,11 @@ export default _this => {
         // cameraTransform.updateMatrix();
         // cameraInstanceMesh.setMatrixAt(index, cameraTransform.matrix);
         // group.add(rangeMesh)
-        // group.add(mesh)
+        group.add(mesh)
       })
-      // group.add( cameraInstanceMesh )
+      // group.add(rangeInstance)
       const newSingleMesh = new THREE.Mesh(singleGeo, cameraMaterial)
-    group.add(newSingleMesh)
+      // group.add(newSingleMesh)
     },
     xhr => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
     error => console.log('An error happened', error)
